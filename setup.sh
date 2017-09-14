14fbc17a22bb91aef7ccbfef09963b1f3697c118
Anant Joshi <anantjoshi1324@gmail.com>
2017-09-14 22:07:46 +0530

    Make changes

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 apt-get -yq update
-apt-get -yq install python-pip python-setuptools libboost-all-dev
+apt-get -yq install python-pip python-setuptools libboost-all-dev cmake
 sudo -H pip install --upgrade pip
 sudo -H pip install numpy
-echo "export PYTHONPATH=\"/usr/include/python2.7/Python.h\"" >> ~/.bashrc
+echo "export PYTHONPATH=\"$PYTHONPATH:/usr/include/python2.7/Python.h\"" >> ~/.bashrc
