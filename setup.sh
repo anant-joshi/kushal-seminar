5469b237ea6972e3a850e14bdf8935c3c7544d53
Anant Joshi <anantjoshi1324@gmail.com>
2017-09-14 21:58:12 +0530

    Update script to reflect requirements

--- setup.sh
+++ setup.sh
@@ -1,1 +1,6 @@
-echo Hello
+#!/bin/bash
+apt-get -yq update
+apt-get -yq install python-pip python-setuptools libboost-all-dev
+sudo -H pip install --upgrade pip
+sudo -H pip install numpy
+echo "export PYTHONPATH=\"/usr/include/python2.7/Python.h\"" >> ~/.bashrc
