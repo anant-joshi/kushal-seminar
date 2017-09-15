5493254a8b31b105048219c605429e5289374bcf
Anant Joshi <anantjoshi1324@gmail.com>
2017-09-15 20:18:48 +0530

    Add support for Fedora/RHEL, and macOS

--- setup.sh
+++ setup.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
-apt-get -yq update
-apt-get -yq install python-pip python-setuptools libboost-all-dev cmake
+if [ $(which apt-get) -eq 0]; then
+    apt-get -yq update
+    apt-get -yq install python-pip python-setuptools libboost-all-dev cmake
+elif [ $(which yum) -eq 0]; then
+    yum clean all
+    yum -y update
+    yum -y install python-pip python-setuptools boost-devel cmake
+elif [ $(which brew) -eq 0]; then
+    brew install boost cmake
+else
+    exit(1)
+fi
 sudo -H pip install --upgrade pip
 sudo -H pip install numpy
-echo "export PYTHONPATH=\"$PYTHONPATH:/usr/include/python2.7/Python.h\"" >> ~/.bashrc
+$PYTHON_H_LOC = $(locate Python.h | head -n 1)
+echo "export PYTHONPATH=\"$PYTHONPATH:$PYTHON_H_LOC\"" >> ~/.bashrc
